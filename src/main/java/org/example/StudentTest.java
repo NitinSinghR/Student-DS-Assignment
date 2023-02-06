0976b29d6c79b4e1886c10e3c9bb7c8785b32689
NitinSinghR <nitin.singh@tringapps.com>
2023-02-06 12:39:53 +0530

    Add files via upload

--- src/main/java/org/example/StudentTest.java
+++ src/main/java/org/example/StudentTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-
 import java.util.*;
 import java.util.logging.*;
 
@@ -51,28 +50,28 @@ public class StudentTest {
 
         String format = " %2s | %-10s | %7s0 ";
 
-        Scanner sc=new Scanner(System.in);
-        Logger l=Logger.getLogger("com.api.jar");
+        Scanner sc = new Scanner(System.in);
+        Logger l = Logger.getLogger("com.api.jar");
 
         l.info("Enter the number of students");
-        int numOfStudent=sc.nextInt();
+        int numOfStudent = sc.nextInt();
 
         LinkedList<Student> studentList = new LinkedList<>();
 
         for (i = 0; i < numOfStudent; i++) {
-            l.log(Level.INFO,()->"Enter the Student ID ");
-            id=sc.next();
-            l.log(Level.INFO,()->"Enter the Student Name ");
-            name=sc.next();
-            l.log(Level.INFO,()->"Enter the Student GPA ");
-            gpa=sc.nextDouble();
-            studentList.add(new Student( id,name,gpa));
+            l.log(Level.INFO, () -> "Enter the Student ID ");
+            id = sc.next();
+            l.log(Level.INFO, () -> "Enter the Student Name ");
+            name = sc.next();
+            l.log(Level.INFO, () -> "Enter the Student GPA ");
+            gpa = sc.nextDouble();
+            studentList.add(new Student(id, name, gpa));
         }
 
         l.info("Before Sorting");
 
         for (i = 0; i < studentList.size(); i++) {
-            String s1=String.format(format,"ID: "+studentList.get(i).getId(), "Name: "+studentList.get(i).getName(), "GPA: "+studentList.get(i).getGPA());
+            String s1 = String.format(format, "ID: " + studentList.get(i).getId(), "Name: " + studentList.get(i).getName(), "GPA: " + studentList.get(i).getGPA());
             l.info(s1);
         }
 
@@ -88,10 +87,9 @@ public class StudentTest {
 
         l.info("After Sorting");
 
-        for(i = 0; i<studentList.size();i++) {
-                String s1 = String.format(format, "ID: " + studentList.get(i).getId(), "Name: " + studentList.get(i).getName(), "GPA: " + studentList.get(i).getGPA());
-                l.info(s1);
-            }
-
+        for (i = 0; i < studentList.size(); i++) {
+            String s1 = String.format(format, "ID: " + studentList.get(i).getId(), "Name: " + studentList.get(i).getName(), "GPA: " + studentList.get(i).getGPA());
+            l.info(s1);
         }
+    }
 }
